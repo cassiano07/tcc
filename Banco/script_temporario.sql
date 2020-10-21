a60ebba28c4c13e2cacd24f7ba4be9d4f44ff445
bruno dirk <bruno_dirk@hotmail.com>
2020-10-21 20:09:42 -0300

    cadastro de usuarios

--- Banco/script_temporario.sql
+++ Banco/script_temporario.sql
@@ -6,8 +6,11 @@ use ea;
 -- criando tabelas
 create table usuarios(
 	usuario_id int(4) auto_increment,
+	nome_completo varchar(250) not null;
+	telefone BIGINT(30);
 	email varchar(100) not null,
 	senha varchar(100) not null,
+	data_criacao datetime;
 	primary key (usuario_id)
 );
 
